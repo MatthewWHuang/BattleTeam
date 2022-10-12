d2c3e60462f245a74fb3032f18221f1bc1c81db4
MatthewWHuang <thematthewhuang@gmail.com>
2022-10-12 17:10:37 -0400

    Sign up page fix

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import login, { signup } from "../api/AuthAPI";
 import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../context/Auth.context";
