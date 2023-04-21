f6e348728ca7e3ef7c36dae268ecefe8aea82ffe
MatthewWHuang <thematthewhuang@gmail.com>
2023-04-21 11:01:30 -0400

    fixed login localstorage

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,69 +4,70 @@ import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../context/Auth.context";
 
 function Login({}) {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [notification, setNotification] = useState("");
-  const { state, logIn, openAdmin } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [notification, setNotification] = useState("");
+    const { state, logIn, openAdmin } = useContext(AuthContext);
 
-  const handleLoginPress = async (e) => {
-    e.preventDefault();
-    const [status, admin] = await login(username, password);
-    if (status === "success") {
-      navigate("/");
-      logIn(username, admin);
-      if (admin) {
-        openAdmin();
-      }
-      localStorage.setItem("username", username);
-      localStorage.setItem("password", password);
-    } else if (status === "incorrect") {
-      setNotification("Username/password incorrect");
-      setPassword("");
-    } else {
-      setNotification("Sorry, something went wrong.");
-    }
-  };
+    const handleLoginPress = async (e) => {
+        e.preventDefault();
+        const [status, admin] = await login(username, password);
+        if (status === "success") {
+            logIn(username, admin);
+            // if (admin) {
+            //     openAdmin();
+            // }
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+            console.log("username saved");
+            navigate("/");
+        } else if (status === "incorrect") {
+            setNotification("Username/password incorrect");
+            setPassword("");
+        } else {
+            setNotification("Sorry, something went wrong.");
+        }
+    };
 
-  useEffect(() => {
-    document.title = "Log In - Battle Team";
-  });
+    useEffect(() => {
+        document.title = "Log In - Battle Team";
+    });
 
-  return (
-    <div>
-      <h1>Log In</h1>
-      <p style={{ color: "red" }}>{notification}</p>
-      <form>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <br />
-        <label htmlFor="pass">Password: </label>
-        <input
-          type="password"
-          id="pass"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <br />
-        <button type="submit" onClick={handleLoginPress}>
-          LOG IN
-        </button>
-      </form>
-      <p>
-        Don't have an account? <Link to="/signup">Sign Up</Link>!
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Log In</h1>
+            <p style={{ color: "red" }}>{notification}</p>
+            <form>
+                <label htmlFor="username">Username: </label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+                <br />
+                <label htmlFor="pass">Password: </label>
+                <input
+                    type="password"
+                    id="pass"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <br />
+                <button type="submit" onClick={handleLoginPress}>
+                    LOG IN
+                </button>
+            </form>
+            <p>
+                Don't have an account? <Link to="/signup">Sign Up</Link>!
+            </p>
+        </div>
+    );
 }
 
 export default Login;
